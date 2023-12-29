2f5955a22416e7ca02afe4acef1cfed409fd86e3
Victor Sales <thehoodgamer20@gmail.com>
2023-12-29 19:56:06 -0300

    melhorado o design dos cards do site

--- src/App.js
+++ src/App.js
@@ -22,7 +22,19 @@ function App() {
     image1={"https://i.ytimg.com/vi/GxTJ2k5cdkA/maxresdefault.jpg"} 
     image2={"https://i.ytimg.com/vi/YEweVHzr_WE/maxresdefault.jpg"} 
     image3={"https://i.ytimg.com/vi/_lRpY9JPPiQ/maxresdefault.jpg"}
-    description1={"Goku SSJ Ultra Red"}></Cards>
+    description1={"Ultra Goku SSJ Red"}
+    description2={"Ultra Goku Ultra Instinct (Omen)"}
+    description3={"Ultra Super Vegetto"}>
+    </Cards>
+    <Cards 
+    image1={"https://steamuserimages-a.akamaihd.net/ugc/2028359009885158301/E4330264EBA1552FD22BBE343F361399D2CF69AD/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"}
+    image2={"https://i.ytimg.com/vi/dxmZ765Rn2M/maxresdefault.jpg"}
+    image3={"https://i.ytimg.com/vi/0lIXTEG0IYA/maxresdefault.jpg"}
+    description1={"Ultra Gogeta Blue"}
+    description2={"Ultra Super Saiyan Gogeta"}
+    description3={"Ultra Vegetto Blue"}
+    >
+    </Cards>
     </div>
   );
 }
